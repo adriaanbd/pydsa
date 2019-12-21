20910a1a54783caa7f1a89918e2ca56699876153
Adriaan Beiertz <adriaanbd@gmail.com>
2019-12-21 11:31:19 -0500

    removing source code links

--- find_files/find_files.py
+++ find_files/find_files.py
@@ -1,11 +1,3 @@
-# https://www.python.org/dev/peps/pep-0428/#directory-walking
-# https://docs.python.org/3/library/pathlib.html
-# https://pathlib.readthedocs.io/en/pep428/
-# https://bitbucket.org/pitrou/pathlib/src/default/pathlib.py
-
-# rglob https://bitbucket.org/pitrou/pathlib/src/1f9672002e3a5e54c48dd48912cc6d45f8a2137d/pathlib.py#lines-1218
-# fundamental functionality here: https://bitbucket.org/pitrou/pathlib/src/1f9672002e3a5e54c48dd48912cc6d45f8a2137d/pathlib.py#lines-638
-
 from pathlib import Path
 
 
@@ -29,8 +21,6 @@ def find_files(suffix: str, path: str, dirs=False) -> list:
 
     return file_list
 
-
-print(find_files('.gitkeep', './testdir'))
 
 if __name__ == '__main__':
     # Tests
