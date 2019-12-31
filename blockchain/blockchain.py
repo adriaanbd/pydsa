15555f6db7995890b6e7f551a7cc172be2a72042
Adriaan Beiertz <adriaanbd@gmail.com>
2019-12-31 15:50:22 -0500

    adding test cases and made chain.chain private

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -60,19 +60,19 @@ class Block:
 
 class Chain:
     def __init__(self):
-        self.chain = []
+        self.__chain = []
 
     def add_block(self, data: str):
         assert isinstance(data, str), 'Data must be a string'
-        assert len(data) > 0, 'Cannot create Block with empty data'
+        assert data and len(data) > 0, 'Cannot create Block with no data'
         block = self._create_block(data)
-        self.chain.append(block)
+        self.__chain.append(block)
 
     def _create_block(self, data) -> Block:
         block = Block(data)
-        block.height = len(self.chain)
-        if len(self.chain) > 0:
-            prev_idx = len(self.chain) - 1
+        block.height = len(self.__chain)
+        if len(self.__chain) > 0:
+            prev_idx = len(self.__chain) - 1
             prev_block = self.get_block_by_height(prev_idx)
             block.prev_hash = prev_block.hash
         block.hash = block.calc_hash()
@@ -80,26 +80,29 @@ class Chain:
 
     def get_block_by_height(self, height: int) -> Block:
         assert isinstance(height, int), 'Height must be an integer'
-        assert 0 <= height < len(self.chain), 'Height is invalid'
-        return self.chain[height]
+        assert 0 <= height < len(self.__chain), 'Height is invalid'
+        return self.__chain[height]
 
     def get_block_by_hash(self, hash_str: str) -> Block:
         assert isinstance(hash_str, str), 'Hash must be a string'
-        for block in self.chain:
+        for block in self.__chain:
             if block.hash == hash_str:
                 return block
 
     def validate_chain(self) -> list:
         errors = []
-        for height, block in enumerate(self.chain):
+        for height, block in enumerate(self.__chain):
             if not block.validate_block():
                 errors.append({
                     'error': 'invalid block',
                     'block': block
                     })
             if height:
-                prev_block = self.chain[height - 1]
-                has_invalid_hashes = prev_block.hash != block.prev_hash
+                prev_block = self.__chain[height - 1]
+                prev_hash, prev_block.hash = prev_block.hash, None
+                current_hash = prev_block.calc_hash()
+                prev_block.hash = prev_hash
+                has_invalid_hashes = current_hash != block.prev_hash
                 if has_invalid_hashes:
                     errors.append({
                         'error': 'discrepancy between blocks',
@@ -108,8 +111,77 @@ class Chain:
                         })
         return errors
 
+    def __iter__(self):
+        for block in self.__chain:
+            yield block
+
     def __repr__(self):
-        return f'{self.chain}'
+        return f'{self.__chain}'
 
     def __str__(self):
-        return f'{self.chain}'
+        return f'{self.__chain}'
+
+
+if __name__ == "__main__":
+    def test_add_block_empty_string():
+        chain = Chain()
+        try:
+            chain.add_block('')
+        except AssertionError as error:
+            assert isinstance(error, AssertionError)
+        else:
+            raise AssertionError('Should raise AssertionError')
+
+    def test_add_block_with_none():
+        chain = Chain()
+        try:
+            chain.add_block(None)
+        except AssertionError as error:
+            assert isinstance(error, AssertionError)
+        else:
+            raise AssertionError('Should raise AssertionError')
+
+    def test_add_block_with_integer():
+        chain = Chain()
+        try:
+            chain.add_block(1)
+        except AssertionError as error:
+            assert isinstance(error, AssertionError)
+        else:
+            raise AssertionError('Should raise AssertionError')
+
+    def test_chain_validation_returns_errors():
+        chain = Chain()
+        chain.add_block('Alpha')
+        chain.add_block('Bravo')
+        chain.add_block('Charlie')
+        bravo = chain.get_block_by_height(1)
+        bravo.height = 5
+        errors = chain.validate_chain()
+        assert len(errors) == 2, 'Validation failed to find invalid block'
+
+    def test_chain_validation_returns_no_errors():
+        chain = Chain()
+        chain.add_block('Alpha')
+        chain.add_block('Bravo')
+        chain.add_block('Charlie')
+        errors = chain.validate_chain()
+        assert len(errors) == 0, 'Validation failed with false positive'
+
+    def test_block_hashes_are_different():
+        chain = Chain()
+        chain.add_block('Alpha')
+        chain.add_block('Bravo')
+        chain.add_block('Charlie')
+        for height, block in enumerate(chain):
+            if height:
+                prev_block = chain.get_block_by_height(height - 1)
+                assert prev_block.hash != block.hash, 'Hashes should differ'
+
+    # run tests
+    test_add_block_empty_string()
+    test_add_block_with_none()
+    test_add_block_with_integer()
+    test_chain_validation_returns_errors()
+    test_chain_validation_returns_no_errors()
+    test_block_hashes_are_different()
